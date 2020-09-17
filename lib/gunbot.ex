928332a0b3ad4fd13dce76670246bf6459544d1a
Brandt Hill <brandtlarsonhill@gmail.com>
2020-09-17 22:59:30 +0000

    reduce boilerplate

--- lib/gunbot.ex
+++ lib/gunbot.ex
@@ -4,23 +4,15 @@ defmodule Gunbot do
   """
 
   use Application
-  use Supervisor
 
   def start(_type, _args) do
-    start_link([])
-  end
-
-  def start_link(opts) do
-    Supervisor.start_link(__MODULE__, :ok, opts)
-  end
-
-  def init(:ok) do
     children = [
       Gunbot.Consumer,
       Gunbot.Repo,
       Gunbot.Monitor
     ]
-    Supervisor.init(children, strategy: :one_for_one)
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: Gunbot.Supervisor)
   end
 
 end
