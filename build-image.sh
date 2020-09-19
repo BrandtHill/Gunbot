f6bac06e88bf5ecd2eb9eba9c73842a387517644
Brandt Hill <brandtlarsonhill@gmail.com>
2020-09-19 18:34:57 +0000

    Add www to gui url for default configs

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -i -v "`pwd`/:/app" -w "/app" elixir:1.10-alpine sh -c "mix local.hex --force && mix local.rebar --force && mix deps.get && MIX_ENV=prod mix release --overwrite"
+docker run --rm -i -v "`pwd`/:/app" -w "/app" elixir:1.10-alpine sh -c "mix local.hex --force && mix local.rebar --force && mix deps.get && mix deps.compile && MIX_ENV=prod mix release --overwrite"
 
 docker build -t "brandt/gunbot:latest" .
