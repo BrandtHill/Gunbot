833c9a7fa7991782212b2c8c4051d51511ff35d1
Brandt Hill <brandtlarsonhill@gmail.com>
2022-05-05 22:48:58 +0000

    update elixir version in docker build script

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -i -v "`pwd`/:/app" -w "/app" elixir:1.10-alpine sh -c "apk add git && mix local.hex --force && mix local.rebar --force && mix deps.get && mix deps.compile && MIX_ENV=prod mix release --overwrite"
+docker run --rm -i -v "`pwd`/:/app" -w "/app" elixir:1.13-alpine sh -c "apk add git && mix local.hex --force && mix local.rebar --force && mix deps.get && mix deps.compile && MIX_ENV=prod mix release --overwrite"
 
 docker build -t "brandt/gunbot:latest" .
